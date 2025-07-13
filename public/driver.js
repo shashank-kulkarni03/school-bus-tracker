ea7236adaccba753ab5b75a19d879cb3e27bbf96
Shashank Kulkarni <shashankkulkarniofficial@gmail.com>
2025-07-14 01:42:17 +0530

    updated driver.js

--- public/driver.js
+++ public/driver.js
@@ -10,6 +10,19 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 let routingControl = null;
 const schoolLatLng = L.latLng(13.1535, 77.614);
+
+const schoolIcon = L.icon({
+  iconUrl: "https://cdn-icons-png.flaticon.com/512/167/167707.png",
+  iconSize: [35, 35],
+  iconAnchor: [17, 35],
+  popupAnchor: [0, -30],
+});
+
+L.marker(schoolLatLng, { icon: schoolIcon })
+  .addTo(map)
+  .bindPopup("<b>🏫 Sir MVIT (School)</b>")
+  .openPopup();
+
 let driverMarker = null;
 
 // 2-opt optimization function
